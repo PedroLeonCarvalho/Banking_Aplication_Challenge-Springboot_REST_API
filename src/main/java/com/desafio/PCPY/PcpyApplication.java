7a7a34b059dd277138c46b26da8f1ec5dd0b0330
Pedro Leon <pedrolbbcarvalho@gmail.com>
2024-02-24 19:17:28 -0300

    first commit

---
+++ src/main/java/com/desafio/PCPY/PcpyApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.PCPY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcpyApplication.class, args);
+	}
+
+}
