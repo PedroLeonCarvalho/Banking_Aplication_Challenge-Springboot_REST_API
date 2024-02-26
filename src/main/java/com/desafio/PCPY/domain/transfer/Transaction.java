8e5a4a487f84b8c5c9944131c1e6c9224b0d6e34
Pedro Leon <pedrolbbcarvalho@gmail.com>
2024-02-26 19:29:17 -0300

    Transaction Entity

---
+++ src/main/java/com/desafio/PCPY/domain/transfer/Transaction.java
@@ -0,0 +1,39 @@
+package com.desafio.PCPY.domain.transfer;
+
+import com.desafio.PCPY.domain.usuarios.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private BigDecimal amount;
+
+    private LocalDateTime timestamp;
+
+
+
+
+}
